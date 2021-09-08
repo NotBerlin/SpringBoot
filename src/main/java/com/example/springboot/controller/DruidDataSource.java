0cc86326224953867f8101a89329a8460326a5fb
chenluolin <917988385@qq.com>
2021-09-08 18:39:49 +0800

    feat: 返回数据

--- src/main/java/com/example/springboot/controller/DruidDataSource.java
+++ src/main/java/com/example/springboot/controller/DruidDataSource.java
@@ -5,13 +5,13 @@ import java.sql.SQLException;
 
 public class DruidDataSource {
     public static void main(String[] args) throws SQLException {
-        com.alibaba.druid.pool.DruidDataSource druidDataSource = new com.alibaba.druid.pool.DruidDataSource();
-        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        druidDataSource.setUrl("jdbc:mysql://localhost:3306/test");
-        druidDataSource.setUsername("root");
-        druidDataSource.setPassword("password");
-        Connection connection = druidDataSource.getConnection();
-        System.out.println(connection);
-        connection.close();
+//        com.alibaba.druid.pool.DruidDataSource druidDataSource = new com.alibaba.druid.pool.DruidDataSource();
+//        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        druidDataSource.setUrl("jdbc:mysql://localhost:3306/test");
+//        druidDataSource.setUsername("root");
+//        druidDataSource.setPassword("password");
+//        Connection connection = druidDataSource.getConnection();
+//        System.out.println(connection);
+//        connection.close();
     }
 }
--- src/main/java/com/example/springboot/controller/HelloCtroller.java
+++ src/main/java/com/example/springboot/controller/HelloCtroller.java
@@ -1,23 +1,39 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.dao.Data;
+import com.example.springboot.entity.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.Map;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloCtroller {
 
-    @GetMapping("/test")
-    public Data test(HttpServletRequest reqest){
+    @GetMapping(value = "/test")
+    public Data test(HttpServletRequest reqest,
+                     String userName) throws JsonProcessingException {
 //        Object data = new Object();
+        // 创建map对象
         HashMap<String, String> data = new HashMap<String, String>();
         data.put("weather", "晴朗");
         data.put("color", "黄色");
-        Data result = new Data("操作成功",data , 0);
+
+        // 使用entity对象
+        User user = new User();
+        user.setUserName("张三");
+        user.setAge(18);
+
+        // 将对象转json字符串
+        ObjectMapper objectMapper = new ObjectMapper();
+        String s = objectMapper.writeValueAsString(user);
+
+        Data result = new Data("操作成功",s , 0);
         return result;
     }
 }
