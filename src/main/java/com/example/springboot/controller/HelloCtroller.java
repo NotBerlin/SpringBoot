d6b1b5f3dd6da3412ab4c921a55ccf6e2aa08839
chenluolin <917988385@qq.com>
2021-09-10 17:54:43 +0800

    feat: spring传参

--- src/main/java/com/example/springboot/controller/HelloCtroller.java
+++ src/main/java/com/example/springboot/controller/HelloCtroller.java
@@ -7,8 +7,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -55,10 +59,47 @@ public class HelloCtroller {
         return result;
     }
 
-    @PostMapping(value = "/test3")
-    public Data test3(@RequestBody VO<User> vo) throws JsonProcessingException {
+    @PostMapping(value = "/test4")
+    public Data test4(@RequestBody List<User> vo) throws JsonProcessingException {
         System.out.println(vo.toString());
         Data result = new Data("操作成功",vo , 0);
         return result;
+    }
+
+    @PostMapping(value = "/test5")
+    public Data test5(@RequestBody List<User> vo) throws JsonProcessingException {
+        System.out.println(vo.toString());
+        Data result = new Data("操作成功",vo , 0);
+        return result;
+    }
+
+    @GetMapping(value = "/test6")
+    public Data test6(@RequestParam(value = "name", required = false, defaultValue = "zhangsan") String username) throws JsonProcessingException {
+        System.out.println(username);
+        Data result = new Data("操作成功",username , 0);
+        return result;
+    }
+
+    @GetMapping(value = "/test7/{name}")
+    public Data test7(@PathVariable(value = "name", required = false) String username) throws JsonProcessingException {
+        System.out.println(username);
+        Data result = new Data("操作成功",username , 0);
+        return result;
+    }
+
+    @GetMapping(value = "/test8")
+    public Data test8(String date) throws JsonProcessingException {
+        System.out.println(date);
+        Data result = new Data("操作成功",date , 0);
+        return result;
+    }
+
+    @PostMapping(value = "/test9")
+    public void test9(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+    }
+
+    @GetMapping(value = "/test10")
+    public void test10(@RequestHeader(value = "User-agent", required = true) String userAgent) {
+        System.out.println(userAgent);
     }
 }
